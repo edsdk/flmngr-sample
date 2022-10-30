3cd6f5ccd72ccf579123259e84f13c7a5dde183f
dmitriy-komarov <dmitriy-komarov@3627127.no-reply.drupal.org>
2022-10-30 15:01:30 +0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import {Flmngr} from "flmngr";
+
+Flmngr.load("FLMNFLMN", {
+    onFlmngrAndImgPenLoaded: () => {
+        let elBtn = document.getElementById("btn");
+
+        // Style button as ready to be pressed
+        elBtn.style.opacity = "1";
+        elBtn.style.cursor = "pointer";
+        var elLoading = document.getElementById("loading");
+        elLoading.parentElement.removeChild(elLoading);
+
+        // Add a listener for selecting files
+        elBtn.addEventListener("click", function() {
+            selectFiles();
+        });
+    }
+});
+
+function selectFiles() {
+
+    Flmngr.open({
+        urlFileManager: 'https://fm.flmngr.com/fileManager',
+        urlFiles: 'https://fm.flmngr.com/files',
+        isMultiple: false,
+        acceptExtensions: ["png", "jpg", "jpeg", "gif", "webp"],
+        onFinish: (files) => {
+            showSelectedImage(files);
+        }
+    });
+
+}
+
+function showSelectedImage(files) {
+    let elImages = document.getElementById("images");
+    elImages.innerHTML = "";
+
+    var file = files[0];
+
+    let el = document.createElement("div");
+    el.className = "image";
+    elImages.appendChild(el);
+
+    let elImg = document.createElement("img");
+    elImg.src = file.url;
+    elImg.alt = "Image selected in Flmngr";
+    el.appendChild(elImg);
+
+    let elP = document.createElement("p");
+    elP.textContent = file.url;
+    el.appendChild(elP);
+}
